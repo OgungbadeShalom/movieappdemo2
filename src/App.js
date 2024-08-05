9dfe7efbc533daf9d613dd4d08ff46afc0121676
Shalom{code} <114452576+OgungbadeShalom@users.noreply.github.com>
2024-08-05 01:45:47 +0100

    Update App.js
    changed the http to htttps
    
    something about security

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg";
 import "./App.css";
 
 // API URL for the movie app
-const API_URL = 'http://www.omdbapi.com?apikey=b9c593b';
+const API_URL = 'https://www.omdbapi.com?apikey=b9c593b';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
